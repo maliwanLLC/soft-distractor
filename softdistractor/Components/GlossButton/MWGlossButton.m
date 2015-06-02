4fbbc903c69262e84275b6631d14ccb45ee2451c
Nick Savula <mykola.savula@gmail.com>
2015-06-02 18:27:17 +0300

    - code grooming

--- softdistractor/Components/GlossButton/MWGlossButton.m
+++ softdistractor/Components/GlossButton/MWGlossButton.m
@@ -43,38 +43,6 @@
     return self;
 }
 
-- (CGPathRef)pathForRoundedRect:(CGRect)rect radius:(CGFloat)radius
-{
-    CGMutablePathRef retPath = CGPathCreateMutable();
-    
-    CGRect innerRect = CGRectInset(rect, radius, radius);
-    
-    CGFloat inside_right = innerRect.origin.x + innerRect.size.width;
-    CGFloat outside_right = rect.origin.x + rect.size.width;
-    CGFloat inside_bottom = innerRect.origin.y + innerRect.size.height;
-    CGFloat outside_bottom = rect.origin.y + rect.size.height;
-    
-    CGFloat inside_top = innerRect.origin.y;
-    CGFloat outside_top = rect.origin.y;
-    CGFloat outside_left = rect.origin.x;
-    
-    CGPathMoveToPoint(retPath, NULL, innerRect.origin.x, outside_top);
-    
-    CGPathAddLineToPoint(retPath, NULL, inside_right, outside_top);
-    CGPathAddArcToPoint(retPath, NULL, outside_right, outside_top, outside_right, inside_top, radius);
-    CGPathAddLineToPoint(retPath, NULL, outside_right, inside_bottom);
-    CGPathAddArcToPoint(retPath, NULL,  outside_right, outside_bottom, inside_right, outside_bottom, radius);
-    
-    CGPathAddLineToPoint(retPath, NULL, innerRect.origin.x, outside_bottom);
-    CGPathAddArcToPoint(retPath, NULL,  outside_left, outside_bottom, outside_left, inside_bottom, radius);
-    CGPathAddLineToPoint(retPath, NULL, outside_left, inside_top);
-    CGPathAddArcToPoint(retPath, NULL,  outside_left, outside_top, innerRect.origin.x, outside_top, radius);
-    
-    CGPathCloseSubpath(retPath);
-    
-    return retPath;
-}
-
 - (void)setupDefaults
 {
     // create emitter layer
@@ -87,14 +55,14 @@
     self.emitterLayer.renderMode = kCAEmitterLayerAdditive;
     self.emitterLayer.emitterSize = CGSizeMake(self.frame.size.width / 4.f, self.frame.size.height / 4.f);
     
-    // create the ball emitter cell
+    // create emitter cell
     CAEmitterCell *fire = [CAEmitterCell emitterCell];
     fire.color = [[UIColor lightGrayColor] CGColor];
     fire.contents = (id)[[UIImage imageNamed:@"fire"] CGImage];
     fire.birthRate = 100;
     fire.lifetime = 18;
     fire.velocity = 2;
-    fire.emissionRange = 0.01;// M_PI / 2.f;
+    fire.emissionRange = 0.01;
     [fire setName:@"ball"];
     
 #ifdef BLACK_HOLE
